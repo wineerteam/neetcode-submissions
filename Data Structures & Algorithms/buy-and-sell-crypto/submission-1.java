d8c867ad6635ec2f19aa57ca7b23b2ce5047ee15
SUNIL KUMAR YADAV <sunilemail10@gmail.com>
2026-05-11 17:01:44 +0530

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxProfit(int[] arr) {
+
+        int minprice=Integer.MAX_VALUE;
+        int maxprofit=0;
+        int ans=0;
+
+        for( int i=0; i<arr.length; i++ ){
+            minprice=Math.min(arr[i],minprice);
+            maxprofit=Math.max(maxprofit,arr[i]-minprice);
+
+        }
+        return maxprofit;
+    }
+}
