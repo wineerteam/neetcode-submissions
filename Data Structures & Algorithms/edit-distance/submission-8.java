b608b5246a902fdf1d72d30e6e129556235ef800
SUNIL KUMAR YADAV <sunilemail10@gmail.com>
2026-06-22 14:11:57 +0530

    Add: edit-distance - submission-8

---
+++ Data Structures & Algorithms/edit-distance/submission-8.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int minDistance(String s1, String s2) {
+
+        int n = s1.length();
+        int m = s2.length();
+        int dp[][] = new int[n+1][m+1];
+
+        for( int i=0; i<=n; i++){
+            for( int j=0; j<=m; j++){
+                if( i==0){
+                dp[i][j]=j;
+                continue; 
+                }
+
+                if( j==0){
+                dp[i][j]=i;
+                continue;
+}
+                if( s1.charAt(i-1) == s2.charAt(j-1))
+                dp[i][j]=dp[i-1][j-1];
+                else{
+                    dp[i][j]=1+Math.min(dp[i-1][j],Math.min(dp[i][j-1],dp[i-1][j-1]));
+                }
+            }
+           
+        }
+         return dp[n][m];
+    }
+}
